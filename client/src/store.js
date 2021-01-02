461e51f1fced26c61051c5d422fa758ddfc3ade5
debo9210 <omololuadebowale1@gmail.com>
2021-01-02 02:11:45 -0500

    Created and linked app to redux store

---
+++ client/src/store.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const reducers = combineReducers({});
+const initialState = {};
+
+const middleware = [thunk];
+
+const devTools =
+  process.env.NODE_ENV === 'production'
+    ? applyMiddleware(...middleware)
+    : composeWithDevTools(applyMiddleware(...middleware));
+
+const store = createStore(reducers, initialState, devTools);
+
+export default store;
