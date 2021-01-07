6e443b3d771fd18bdd09c5601bc9459a76814ee9
debo9210 <omololuadebowale1@gmail.com>
2021-01-07 04:36:47 -0500

    prepared app for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const imageUploadApi = require('./routes/api/imageUpload');
 
@@ -16,11 +17,21 @@ mongoose
   .then(() => console.log('connected to database'))
   .catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Unsplash App (:');
-});
+// app.get('/', (req, res) => {
+//   res.send('Welcome to Unsplash App (:');
+// });
 
 app.use('/api/unsplash-app', imageUploadApi);
+
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
